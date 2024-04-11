4a810adbbf443cb039c418ed281c3a7dd43dd1ad
Joydeep Ghosh <joydeepg103@gmail.com>
2024-04-12 03:54:35 +0530

    Tailwind config commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-
-
-
-
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 module.exports = withMT({
   content: [
-  "./index.html",
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-],
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+      boxShadow: {
+        'custom': 'inset 0px 1px 0px 0px rgba(255, 255, 255, 0.4), inset 0px -4px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 4px rgba(255, 255, 255, 0.2), 0px 0px 180px 0px #9917FF'
       },
+      backgroundImage: theme => ({
+        'gradient-to-b': 'linear-gradient(0deg, #A47CF3, #683FEA)'
+      }),
+      scale: {
+        '110': '1.1'
+      },
+      translate: {
+        '-1': '-2px'
+      }
     },
   },
   plugins: [],
 });
-// module.exports = {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
