fe325bacd6f7d8c299d5853e193161cb6b55c11d
Joydeep Ghosh <joydeepg103@gmail.com>
2024-04-14 19:30:05 +0530

    Z-Index

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
 
       {/* Signup form modal */}
       {showSignup && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 " style={{zIndex:"100"}}>
           <div className="bg-white p-8 rounded-lg max-w-md relative">
             <button onClick={() => setShowSignup(false)} className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
